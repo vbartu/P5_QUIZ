39c06825223026d0458103b81f0a00e70206e8f8
vbartu <vicentebartualf@gmail.com>
2018-05-04 01:53:02 +0200

    v1

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var {models} = require('../model/model.js');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.get('/credits', function(req, res) {
+  res.render('credits', { title: 'Express' });
+});
+
+router.get('/quizzes', function(req, res) {
+  models.quiz.findAll()
+  .then(quizzes => {
+    res.render('quizzes', {quizzes : quizzes})
+  });
+});
+
+module.exports = router;
